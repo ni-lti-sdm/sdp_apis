ef18441c69d77d24763377c8f3a0ac34b60e478d
Daren Wilson <daren.wilson@gmail.com>
2020-04-13 22:45:12 -0500

    Change kafka producer to write to topic ingest-phase-1

--- apps/kafka_producer/lib/kafka_producer.ex
+++ apps/kafka_producer/lib/kafka_producer.ex
@@ -1,15 +1,12 @@
 defmodule KafkaProducer do
-
   def write_event(event \\ %{}) do
-    KafkaEx.produce(
-      %KafkaEx.Protocol.Produce.Request{
-        topic: "mw-first-topic",
-        # partition: 0,
-        required_acks: 1,
-        messages: [
-          %KafkaEx.Protocol.Produce.Message{value: Jason.encode!(event)}
-        ]
-      }
-    )
+    KafkaEx.produce(%KafkaEx.Protocol.Produce.Request{
+      topic: "ingest-phase-1",
+      # partition: 0,
+      required_acks: 1,
+      messages: [
+        %KafkaEx.Protocol.Produce.Message{value: Jason.encode!(event)}
+      ]
+    })
   end
 end
